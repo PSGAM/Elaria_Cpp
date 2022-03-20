aff6c110aa5a866354502c5d62ac35c714d50cb5
Pablo Sabate <PabloSGuerrero@gmail.com>
2022-03-20 22:28:47 +0100

    Point of Interest system
    - Starting the Point of Interest system, where an ActorComponent will handle the collision and assigning the corresponding tag.

---
+++ Source/Elaria/AC_PointOfInterest_C.cpp
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AC_PointOfInterest_C.h"
+
+// Sets default values for this component's properties
+UAC_PointOfInterest_C::UAC_PointOfInterest_C()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// Create a BoxComponent that will have the collision channel for "MessageSystemChannel" set to Block, so the 
+	//   traces for the near actors with a message systems component will respond.
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("PointOfInterestBoxCollision"));
+	BoxComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	BoxComponent->SetCollisionResponseToChannel(ECollisionChannel::ECC_GameTraceChannel3, ECollisionResponse::ECR_Block);
+	BoxComponent->SetGenerateOverlapEvents(true);
+	BoxComponent->SetRelativeLocation(FVector(0, 0, 0));
+	BoxComponent->SetRelativeScale3D(FVector(1.0f, 1.0f, 1.0f));
+}
+
+
+// Called when the game starts
+void UAC_PointOfInterest_C::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Destroy other Point of Interest Box Collisions
+	TInlineComponentArray<UActorComponent*> OwnersComponents;
+	UActorComponent* currentComponentBeingChecked;
+	GetOwner()->GetComponents(OwnersComponents);
+	if (OwnersComponents.Num() > 0) {
+		for (int32 CompIdx = 0; CompIdx < OwnersComponents.Num(); CompIdx++)
+		{
+			currentComponentBeingChecked = OwnersComponents[CompIdx];
+
+			if (currentComponentBeingChecked->GetName() == "PointOfInterestBoxCollision")
+			{
+				if (currentComponentBeingChecked != BoxComponent)
+				{
+					currentComponentBeingChecked->DestroyComponent();
+					UE_LOG(LogTemp, Warning, TEXT("PointOfInterest BOX Component has been destroyed successfully."));
+				}
+			}
+		}
+	}
+
+	BoxComponent->AttachTo(GetOwner()->GetRootComponent());
+	BoxComponent->SetRelativeLocation(FVector(0, 0, 0));
+	BoxComponent->SetRelativeRotation(FRotator(0, 0, 0));
+	BoxComponent->SetRelativeScale3D(FVector(0, 0, 0));
+
+	// Add a unique tag in case we want to find if it has a Message System component.
+	GetOwner()->Tags.AddUnique(PointOfInterestComponent_TagName);
+
+}
+
+
+// Called when the component is going to be destroyed, used for clean up on the owner actor.
+void UAC_PointOfInterest_C::EndPlay(EEndPlayReason::Type Reason)
+{
+	if (BoxComponent != nullptr)
+	{
+		BoxComponent->DestroyComponent();
+		BoxComponent->SetActive(false);
+		UE_LOG(LogTemp, Warning, TEXT("MessageSystem Component has been destroyed."));
+	}
+
+	GetOwner()->Tags.Remove(PointOfInterestComponent_TagName);
+}
+
+
+// Called every frame
+void UAC_PointOfInterest_C::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
---
+++ Source/Elaria/AC_PointOfInterest_C.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "Components/ActorComponent.h"
+#include "AC_PointOfInterest_C.generated.h"
+
+
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent), Blueprintable)
+class ELARIA_API UAC_PointOfInterest_C : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UAC_PointOfInterest_C();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	virtual void EndPlay(EEndPlayReason::Type Reason) override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+public:	
+	const FName PointOfInterestComponent_TagName = "PointOfInterestComponent_Tag";
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	UBoxComponent* BoxComponent = nullptr;
+};
